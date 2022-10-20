68879bc69506452c6c9e0743810a09ba45412f2a
chenzhisheng <2143204964@qq.com>
2022-10-20 17:13:00 +0800

    自定义认证逻辑-自定义AuthenticationProvider代替DaoAuthenticationProvider

---
+++ custom-verifycode-authentication/src/main/java/org/javaboy/customverifycodeauthentication/CustomVerifycodeAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package org.javaboy.customverifycodeauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomVerifycodeAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomVerifycodeAuthenticationApplication.class, args);
+    }
+
+}
---
+++ custom-verifycode-authentication/src/test/java/org/javaboy/customverifycodeauthentication/CustomVerifycodeAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaboy.customverifycodeauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomVerifycodeAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
